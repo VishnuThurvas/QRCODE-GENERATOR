fb817b8c037c275d2277a8153724fd7cf8561537
T.S VISHNU <146065352+VishnuThurvas@users.noreply.github.com>
2025-01-06 18:45:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import qrcode
+
+#if not avaialble use " pip install qrcode Image"
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrcode.png")
+    # return img
+
+
+generate_qrcode("github.com")
